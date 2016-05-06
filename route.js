d6e38905402e45cce09ce17604d7a335d61e75e2
teerayut_tan <theerayut_than@hotmail.com>
2016-05-06 16:39:43 +0700

    route

--- route.js
+++ route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var fs = require('read-file');
+
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
@@ -11,6 +13,22 @@ app.get('/home', function (req, res) {
 
 app.get('/contact', function (req, res) {
     res.send('Hi contact theerayut_than@hotmail.com');
+});
+
+app.post('/add', function (req, res) {
+    res.send('POST request to the homepage.');
+});
+
+app.get('/text', function (req, res) {
+    fs.resdFile('./text.txt', function read(err, data) {
+       if(err)
+       {
+           throw err;
+       }
+        var content = data;
+
+        res.send(content);
+    });
 });
 
 app.listen(3000);
